a89792507b4902791f7a75774896d17477d1bf06
kayhan.turhan@gmail.com <kayhan.turhan>
2024-03-08 15:12:58 +0300

    Project commit 2

---
+++ App.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, ImageBackground } from 'react-native';
+import GameStartScreen from './screens/GameStartScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react';
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState(null);
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessCounts, setGuessCounts] = useState(0);
+
+  function sendedNumberHandler(sendedNumber) {
+    setUserNumber(sendedNumber);
+    setGameIsOver(false);
+  }
+
+  function gameOverHandler(numberofGuess) {
+    setGameIsOver(true);
+    setGuessCounts(numberofGuess);
+  }
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessCounts(0);
+  }
+
+  let screen = <GameStartScreen onSendNumber={sendedNumberHandler} />;
+
+  if (userNumber) {
+    screen = (
+      <GameScreen userNumber={userNumber} onGaveOver={gameOverHandler} />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        roundsNumber={guessCounts}
+        userNumber={userNumber}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
+  }
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={['rgba(0,0,0,0.8)', 'transparent']}
+    >
+      <ImageBackground
+        style={styles.container}
+        source={require('./assets/back.jpg')}
+        imageStyle={styles.backImage}
+      >
+        {screen}
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backImage: {
+    opacity: 0.2,
+  },
+});
